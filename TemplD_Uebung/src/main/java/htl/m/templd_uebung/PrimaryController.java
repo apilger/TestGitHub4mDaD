3fd677bf81d6c9c5ad60d3c5b2594a6bf0c246c4
Andreas Pilger <a.pilger@htl-grieskirchen.at>
2021-11-17 20:42:08 +0100

    neues TextField

--- TemplD_Uebung/src/main/java/htl/m/templd_uebung/PrimaryController.java
+++ TemplD_Uebung/src/main/java/htl/m/templd_uebung/PrimaryController.java
@@ -3,11 +3,14 @@ package htl.m.templd_uebung;
 import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
     @FXML
     private Button button;
+    @FXML
+    private TextField textField;
 
   
 }
